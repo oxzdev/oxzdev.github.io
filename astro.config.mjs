4221f83d1a422607fd5f40794c9a001d41eade73
shixiongfei <xiongfei.shi@icloud.com>
2026-08-31 21:34:31 +0800

    Remove solidjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import solidJs from "@astrojs/solid-js";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()],
-
   vite: {
     plugins: [tailwindcss()],
   },
